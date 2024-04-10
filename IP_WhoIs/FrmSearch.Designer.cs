c157e4d3c3288a6cf554616d51f9b6185c9d4a61
Matea Klement <mklement21@foi.hr>
2024-04-10 18:23:08 +0200

    Designed main form FrmSearch for user API input and navigation.

---
+++ IP_WhoIs/FrmSearch.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace IP_WhoIs {
+    partial class FrmSearch {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.pnlSide = new System.Windows.Forms.Panel();
+            this.btnNavSaved = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnNavSearch = new System.Windows.Forms.Button();
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.txtSearchInput = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtAPI = new System.Windows.Forms.TextBox();
+            this.pnlSide.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlHeader.SuspendLayout();
+            this.pnlMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlSide
+            // 
+            this.pnlSide.BackColor = System.Drawing.Color.LightSlateGray;
+            this.pnlSide.Controls.Add(this.btnNavSaved);
+            this.pnlSide.Controls.Add(this.pictureBox1);
+            this.pnlSide.Controls.Add(this.btnNavSearch);
+            this.pnlSide.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlSide.Location = new System.Drawing.Point(0, 44);
+            this.pnlSide.Name = "pnlSide";
+            this.pnlSide.Size = new System.Drawing.Size(241, 607);
+            this.pnlSide.TabIndex = 3;
+            // 
+            // btnNavSaved
+            // 
+            this.btnNavSaved.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnNavSaved.FlatAppearance.BorderSize = 0;
+            this.btnNavSaved.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGray;
+            this.btnNavSaved.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNavSaved.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnNavSaved.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnNavSaved.Location = new System.Drawing.Point(0, 202);
+            this.btnNavSaved.Name = "btnNavSaved";
+            this.btnNavSaved.Size = new System.Drawing.Size(241, 40);
+            this.btnNavSaved.TabIndex = 1;
+            this.btnNavSaved.Text = "Saved";
+            this.btnNavSaved.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(43, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(157, 100);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnNavSearch
+            // 
+            this.btnNavSearch.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnNavSearch.FlatAppearance.BorderSize = 0;
+            this.btnNavSearch.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGray;
+            this.btnNavSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNavSearch.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnNavSearch.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnNavSearch.Location = new System.Drawing.Point(0, 156);
+            this.btnNavSearch.Name = "btnNavSearch";
+            this.btnNavSearch.Size = new System.Drawing.Size(241, 40);
+            this.btnNavSearch.TabIndex = 0;
+            this.btnNavSearch.Text = "Search";
+            this.btnNavSearch.UseVisualStyleBackColor = false;
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.SlateGray;
+            this.pnlHeader.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlHeader.Controls.Add(this.label1);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(1151, 44);
+            this.pnlHeader.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(49, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP Who is";
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Controls.Add(this.txtAPI);
+            this.pnlMain.Controls.Add(this.btnSearch);
+            this.pnlMain.Controls.Add(this.lblSearch);
+            this.pnlMain.Controls.Add(this.txtSearchInput);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.ForeColor = System.Drawing.Color.Transparent;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(1151, 651);
+            this.pnlMain.TabIndex = 5;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSearch.ForeColor = System.Drawing.Color.Black;
+            this.lblSearch.Location = new System.Drawing.Point(288, 110);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(117, 20);
+            this.lblSearch.TabIndex = 1;
+            this.lblSearch.Text = "IP address :";
+            // 
+            // txtSearchInput
+            // 
+            this.txtSearchInput.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtSearchInput.ForeColor = System.Drawing.Color.Black;
+            this.txtSearchInput.Location = new System.Drawing.Point(429, 108);
+            this.txtSearchInput.Multiline = true;
+            this.txtSearchInput.Name = "txtSearchInput";
+            this.txtSearchInput.Size = new System.Drawing.Size(395, 22);
+            this.txtSearchInput.TabIndex = 0;
+            this.txtSearchInput.Text = "https://ipwhois.app/json/193.198.57.183";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnSearch.FlatAppearance.BorderSize = 0;
+            this.btnSearch.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGray;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSearch.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnSearch.Location = new System.Drawing.Point(869, 100);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(178, 41);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // txtAPI
+            // 
+            this.txtAPI.Location = new System.Drawing.Point(305, 188);
+            this.txtAPI.Multiline = true;
+            this.txtAPI.Name = "txtAPI";
+            this.txtAPI.Size = new System.Drawing.Size(772, 369);
+            this.txtAPI.TabIndex = 4;
+            // 
+            // FrmSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1151, 651);
+            this.Controls.Add(this.pnlSide);
+            this.Controls.Add(this.pnlHeader);
+            this.Controls.Add(this.pnlMain);
+            this.Name = "FrmSearch";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IP Who Is";
+            this.pnlSide.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlSide;
+        private System.Windows.Forms.Button btnNavSaved;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnNavSearch;
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.TextBox txtSearchInput;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtAPI;
+    }
+}
+
