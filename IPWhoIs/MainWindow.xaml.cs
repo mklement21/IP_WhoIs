69fc577ee2df6381ea086028c46d482b0602ec0e
Matea Klement <mklement21@foi.hr>
2024-04-29 13:56:15 +0200

    Implemented loading logo from url from ipwhois.io.

--- IPWhoIs/MainWindow.xaml.cs
+++ IPWhoIs/MainWindow.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System.Windows;
+﻿using System.Net.Http;
+using System.Net;
+using System;
+using System.Windows;
 
 namespace IPWhoIs {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            LoadLogoFromUrl();
         }
 
         private void btnSearch_Click(object sender, RoutedEventArgs e) {
@@ -14,6 +18,19 @@ namespace IPWhoIs {
         private void btnSaved_Click(object sender, RoutedEventArgs e) {
             UcSaved ucSaved = new UcSaved();
             contentPanel.Content = ucSaved;
+        }
+
+        public async void LoadLogoFromUrl() {
+            string imageUrl = "https://ipwhois.io/img/logo.svg";
+            try {
+                HttpClient httpClient = new HttpClient();
+
+                string svgData = await httpClient.GetStringAsync(imageUrl);
+
+                webBrowser.NavigateToString(svgData);
+            } catch (Exception ex) {
+                MessageBox.Show($"Error loading SVG: {ex.Message}");
+            }
         }
     }
 }
