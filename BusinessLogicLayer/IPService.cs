9a4a0a68dc9e4f554f66bff8f4798597be1e4976
Matea Klement <mklement21@foi.hr>
2024-07-05 22:10:21 +0200

    Implemented export of address data to a txt file.

--- BusinessLogicLayer/IPService.cs
+++ BusinessLogicLayer/IPService.cs
@@ -2,6 +2,7 @@
 using DataAccessLayer.Repositories;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -60,10 +61,41 @@ namespace BusinessLogicLayer {
             return isSuccessful;
         }
 
-        public bool TxtExpotData(IpAdrese address, string) {
-            bool isSuccessful = false;
-
-            return isSuccessful;
+        public bool TxtExpotData(IpAdrese address, string filePath) {
+            try {
+                using (StreamWriter streamWriter = new StreamWriter(filePath)) {
+                    streamWriter.WriteLine($"IP: {address.IP}");
+                    streamWriter.WriteLine($"Success: {address.success}");
+                    streamWriter.WriteLine($"Type: {address.type}");
+                    streamWriter.WriteLine($"Continent: {address.continent}");
+                    streamWriter.WriteLine($"Continent Code: {address.continent_code}");
+                    streamWriter.WriteLine($"Country: {address.country}");
+                    streamWriter.WriteLine($"Country Code: {address.country_code}");
+                    streamWriter.WriteLine($"Country Capital: {address.country_capital}");
+                    streamWriter.WriteLine($"Country Phone: {address.country_phone}");
+                    streamWriter.WriteLine($"Country Neighbours: {address.country_neighbours}");
+                    streamWriter.WriteLine($"Region: {address.region}");
+                    streamWriter.WriteLine($"City: {address.city}");
+                    streamWriter.WriteLine($"Latitude: {address.latitude}");
+                    streamWriter.WriteLine($"Longitude: {address.longitude}");
+                    streamWriter.WriteLine($"ASN: {address.asn}");
+                    streamWriter.WriteLine($"Org: {address.org}");
+                    streamWriter.WriteLine($"ISP: {address.isp}");
+                    streamWriter.WriteLine($"Timezone: {address.timezone}");
+                    streamWriter.WriteLine($"Timezone Name: {address.timezone_name}");
+                    streamWriter.WriteLine($"Timezone DST Offset: {address.timezone_dstOffset}");
+                    streamWriter.WriteLine($"Timezone GMT Offset: {address.timezone_gmtOffset}");
+                    streamWriter.WriteLine($"Currency: {address.currency}");
+                    streamWriter.WriteLine($"Currency Code: {address.currency_code}");
+                    streamWriter.WriteLine($"Currency Symbol: {address.currency_symbol}");
+                    streamWriter.WriteLine($"Currency Rates: {address.currency_rates}");
+                    streamWriter.WriteLine($"Currency Plural: {address.currency_plural}");
+                    streamWriter.WriteLine($"Favorite: {address.favorite}");
+                }
+                return true;
+            } catch (Exception ex) {
+                return false;
+            }
         }
     }
 }
